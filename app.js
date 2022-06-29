06a177bd772be69fddce558c7fc4609c50c1e076
Jimbei <contrerasr2272@gmail.com>
2022-06-29 16:37:18 -0400

    Generated the HTML file

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//fs allows us to us file sysyem
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+//looks into the process and argv to pull data
+const profileDataArgs = process.argv.slice(2);
+// gives data into profileDataArgs as an array
+const [Name, Github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
+//This const generates the HTML file we are using
+
+fs.writeFile('./index.html', generatePage(Name, Github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+/*const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++){
         console.log(profileDataArr[i]);
     }
@@ -14,3 +27,5 @@ profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
+*/
+

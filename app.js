c5f956cee80f9d0b383c7740be138308381b1766
Jimbei <contrerasr2272@gmail.com>
2022-07-08 13:41:33 -0400

    connected inquirer package to app.js

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+const inquirer = require('inquirer');
+
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
 //fs allows us to us file sysyem
-const fs = require('fs');
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 //looks into the process and argv to pull data
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(Name, Github);
 // gives data into profileDataArgs as an array
-const [Name, Github] = profileDataArgs;
+
 
 //This const generates the HTML file we are using
 
-fs.writeFile('./index.html', generatePage(Name, Github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
+*/
 /*const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++){
         console.log(profileDataArr[i]);
